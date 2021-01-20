a2ec5877c76b435c41910cc991157b6741c9b006
André Baldeweg <kontakt@andrebaldeweg.de>
2021-01-20 12:47:58 +0100

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import store from '../store'
 
 Vue.use(Router)
 
@@ -15,18 +14,6 @@ const router = new Router({
       component: () => import('../views/SearchView'),
     },
     {
-      path: '/book/new',
-      name: 'book.new',
-      component: () => import('../views/SearchView'),
-      props: { showCreateBook: true },
-    },
-    {
-      path: '/book/:bookId',
-      name: 'book',
-      component: () => import('../views/SearchView'),
-      props: true,
-    },
-    {
       path: '*',
       name: 'not-found',
       component: () => import('../views/404View'),
@@ -35,11 +22,6 @@ const router = new Router({
   scrollBehavior() {
     return { x: 0, y: 0 }
   },
-})
-
-router.beforeEach((to, from, next) => {
-  store.commit('app/showOffCanvas', false)
-  next()
 })
 
 export default router
