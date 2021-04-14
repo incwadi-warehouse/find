8a97dc1dc2f6fd21f8bec4ef2c43b36b981b6311
André Baldeweg <kontakt@andrebaldeweg.de>
2021-04-14 19:03:00 +0200

    refactor book details

--- src/components/search/BookShow.vue
+++ src/components/search/BookShow.vue
@@ -1,37 +1,47 @@
 <template>
-  <b-modal :width="900" @close="$emit('close', $event)" v-if="book">
-    <b-container size="m">
-      <h2>{{ book.title }}</h2>
-      <p v-if="book.authorSurname || book.authorFirstname">
-        {{ $t('by') }}
-        {{ formatAuthor(book.authorFirstname, book.authorSurname) }}
-      </p>
-    </b-container>
+  <b-modal :width="1200" @close="$emit('close', $event)" v-if="book">
+    <div class="product">
+      <div class="product_image">
+        <b-container size="m" v-if="book.cover_m && book.cover_l">
+          <img
+            :src="book.cover_m"
+            v-if="!hasZoom"
+            @click="hasZoom = !hasZoom"
+          />
+          <img :src="book.cover_l" v-if="hasZoom" @click="hasZoom = !hasZoom" />
+        </b-container>
+      </div>
 
-    <b-container size="m" v-if="book.cover_m && book.cover_l">
-      <img :src="book.cover_m" v-if="!hasZoom" @click="hasZoom = !hasZoom" />
-      <img :src="book.cover_l" v-if="hasZoom" @click="hasZoom = !hasZoom" />
-    </b-container>
+      <div class="product_details">
+        <b-container size="m">
+          <h2>{{ book.title }}</h2>
+          <p v-if="book.authorSurname || book.authorFirstname">
+            {{ $t('by') }}
+            {{ formatAuthor(book.authorFirstname, book.authorSurname) }}
+          </p>
+        </b-container>
 
-    <b-container size="m" v-if="book.shortDescription">
-      <p>{{ book.shortDescription }}</p>
-    </b-container>
+        <b-container size="m" v-if="book.shortDescription">
+          <p>{{ book.shortDescription }}</p>
+        </b-container>
 
-    <b-container size="m" v-if="book">
-      <p>
-        {{ $t('price') }}: {{ formatPrice(book.price) }}
-        {{ book.currency }}
-      </p>
-      <p>{{ $t('genre') }}: {{ book.genre }}</p>
-      <p>{{ $t('releaseYear') }}: {{ book.releaseYear }}</p>
-      <p>{{ $t('type') }}: {{ $t(book.type) }}</p>
-      <p v-if="book.cond">{{ $t('condition') }}: {{ book.cond }}</p>
-    </b-container>
+        <b-container size="m" v-if="book">
+          <p>
+            {{ $t('price') }}: {{ formatPrice(book.price) }}
+            {{ book.currency }}
+          </p>
+          <p>{{ $t('genre') }}: {{ book.genre }}</p>
+          <p>{{ $t('releaseYear') }}: {{ book.releaseYear }}</p>
+          <p>{{ $t('type') }}: {{ $t(book.type) }}</p>
+          <p v-if="book.cond">{{ $t('condition') }}: {{ book.cond }}</p>
+        </b-container>
 
-    <b-container size="m">
-      <p>{{ $t('branch') }}: {{ book.branchName }}</p>
-      <p :style="{ whiteSpace: 'pre' }">{{ book.branchOrdering }}</p>
-    </b-container>
+        <b-container size="m">
+          <p>{{ $t('branch') }}: {{ book.branchName }}</p>
+          <p :style="{ whiteSpace: 'pre' }">{{ book.branchOrdering }}</p>
+        </b-container>
+      </div>
+    </div>
 
     <br />
     <br />
@@ -51,7 +61,7 @@ export default {
     book: Object,
   },
   setup() {
-    let hasZoom = ref(false)
+    let hasZoom = ref(true)
 
     return { hasZoom }
   },
@@ -61,3 +71,22 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.product_image {
+  width: 200px;
+}
+
+@media all and (min-width: 600px) {
+  .product {
+    display: flex;
+  }
+  .product_image {
+    width: 33%;
+    box-sizing: border-box;
+  }
+  .product_details {
+    flex-grow: 1;
+  }
+}
+</style>
