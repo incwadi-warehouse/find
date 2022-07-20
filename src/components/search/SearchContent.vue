cc0200eed7a5d29571ee40a81fa74b6cd6838c99
André Baldeweg <a@baldeweg.org>
2022-07-20 15:33:16 +0200

    sanitize html

--- src/components/search/SearchContent.vue
+++ src/components/search/SearchContent.vue
@@ -2,13 +2,16 @@
 import { useBranch } from '@/composables/useBranch.js'
 import { computed } from 'vue'
 import { marked } from 'marked'
+import DOMPurify from 'dompurify'
 
 const { branch } = useBranch()
 
 const content = computed(() => {
   if (!branch.value || !branch.value.content) return
 
-  return marked.parse(branch.value.content.replace(/(<([^>]+)>)/gi, ''))
+  return DOMPurify.sanitize(marked.parse(branch.value.content), {
+    ALLOWED_TAGS: ['p', 'a', 'img', 'b', 'strong', 'i', 'em', 'ul', 'li'],
+  })
 })
 </script>
 
